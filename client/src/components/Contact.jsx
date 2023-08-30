21f3cc79a30ac5c95a54f08d77e35f81c4935177
ivanneto02 <ivan.a.neto@hotmail.com>
2023-08-30 09:48:29 -0700

    Added resume to website

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -13,6 +13,7 @@ class Contact extends Component {
                     <li className="icon solid fa-phone">(909) 419-2199</li>
                     <li className="icon solid fa-home"><a href="https://www.google.com/maps/place/Riverside,+CA/">Riverside, California, United States</a></li>
                     <li className="icon brands fa-linkedin"><a href="https://www.linkedin.com/in/ivan-neto/">Linkedin</a><br /></li>
+                    <li className="icon fa-file"><a href="https://docs.google.com/document/d/1jAV2I_vfuYwBs0TQp1K_vSsTDDuy1b4_SeKbBFXEaEc/edit?usp=sharing">Resume</a><br /></li>
                 </ul>
             </section>
         );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,6 +5,7 @@ class Header extends Component {
         return (
             <header id="header">
                 <ul className="icons">
+                    <li><a href="https://docs.google.com/document/d/1jAV2I_vfuYwBs0TQp1K_vSsTDDuy1b4_SeKbBFXEaEc/edit?usp=sharing" className="icon fa-file"> Resume<span className="label">Linkedin</span></a></li>
                     <li><a href="https://www.linkedin.com/in/ivan-neto/" className="icon brands fa-linkedin"> Linkedin<span className="label">Linkedin</span></a></li>
                 </ul>
             </header>
