c044bf57e1836c8d419f052f4f430e08ce1153a2
Ivan Neto <ivan.a.neto@hotmail.com>
2023-11-27 15:40:43 -0800

    Adding content to /tutoring

--- client/src/pages/services/Tutoring.jsx
+++ client/src/pages/services/Tutoring.jsx
@@ -13,38 +13,95 @@ class Tutoring extends Component {
 
                         <h1>Tutoring</h1>
                     </section>
+
                     <section>
                         <h2>Introduction</h2>
 
-                        <div>
+                        <p>
                             Tutoring is a huge part of my academic, professional, and personal life.
                             Ever since High School, I've been tutoring my friends and peers for any subject they
                             may be struggling with.
-                        </div>
+                        </p>
 
+                        <p>
+                            Currently I attend the University of California, Riverside as a fourth-year major in
+                            Computer Science. I have found myself in a perfect place to keep doing tutoring work
+                            for friends, family, and peers.
+                        </p>
+                    </section>
+
+                    <section>
+                        <h2>Educational Background</h2>
+
+                        <h3>Relevant High School Education</h3>
+
+                        <p>
+                            In High School, I had the opportunity to partake in AP Calculus AB and AP Calculus BC.
+                            These two classes really expanded my knowledge of mathematics at the time. Until that point,
+                            I had only had experience with High School level arithmetic, geometry, and trigonometry. But
+                            through the help of my amazing teacher <b>Don Tulac</b>, I was able to learn many amazing
+                            Calculus concepts that very properly propped me up to be successful in Computer Science.
+                        </p>
+
+                        <h3>Relevant University Education</h3>
+
+                        <p>
+                            All throughout my stay at the <b>University of California, Riverside</b> I have enjoyed many
+                            courses that have expanded my knowledge of mathematics and Computer Science in radical ways.
+                            For some honorable mentions, I really enjoyed the CS150 - Automata Theory - course by Professor <b>Paea LePendu</b>. I thoroughly enjoyed the way Prof. LePendu teaches and the many applications
+                            that finite-state automata give us. To expand on this, CS120B was a course that I enjoyed, and it
+                            is taught by Professor <b>Phillip Brisk</b>. I created a Youtube channel with all the amazing labs
+                            and programs that we were assigned in this course, which teaches us how to use finite-state automata, or
+                            "state machines" to create state-based programs in arduino. <a href="https://youtu.be/cUCjWoV4INg"><b>Here</b></a> is
+                            an example video of my final project for that class.
+                        </p>
+
+                        <p></p>
+
+                    </section>
+
+                    <section>
+                        <h2>Areas of Expertise</h2>
+
+                        <h3>Data Structures and Algorithms</h3>
+
+                        <p>
+                            My journey with Data Structures and Algorithms began with my very first lecture at UCR, which was my CS010A course, or
+                            "Introduction to Programming in C/C++". The reason why I say this is my introduction to Data Structures is because <b>C++</b> became
+                            my favorite programming language throughout the years. It is the language that I most enjoy programming with,
+                            because of its versatility and low-level access. Although I also enjoy other programming languages such as <b>Python</b>
+                            , <b>Javascript</b>, <b>Java</b>, among others, there is a sense of control that is lacking from these that is ever present
+                            in <b>C++</b>. Without this language, I never would have continued my love for Data Structures and Algorithms concepts that I
+                            learned in subsequent courses.
+                        </p>
+
+                        <p>
+                            CS010B and CS010C served to strengthen my understanding of C/C++, as well as many data structures concepts. Mainly, I learned the
+                            intricacies of Object-Oriented Programming with exercises such as dynamic typing with pointers and memory allocated on the heap.
+                        </p>
+
+                        <h3>Machine Learning and Data Science</h3>
+
+                        <p>
+                           
+                        </p>
 
 
                     </section>
 
                     <section>
-                        test
+                        <h2>Teaching Philosophy</h2>
                     </section>
 
                     <section>
-                        test
+                        <h2>Tutoring Experience</h2>
                     </section>
 
                     <section>
-                        test
+                        <h2>Methods and Techniques</h2>
                     </section>
 
-                    <section>
-                        test
-                    </section>
 
-                    <section>
-                        test
-                    </section>
                 </div>
             </div>
         );
