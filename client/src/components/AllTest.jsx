cd3e2721e2005cf7b610957be69ddb8728da9d64
Ivan Neto <ivan.a.neto@hotmail.com>
2023-11-27 19:31:00 -0800

    Adding alternative to img

--- client/src/components/AllTest.jsx
+++ client/src/components/AllTest.jsx
@@ -8,7 +8,7 @@ class AllTest extends Component {
 						<div className="inner">
 								<header id="header">
 									<ul className="icons">
-										<li><a href="https://www.linkedin.com/in/ivan-neto/" className="icon brands fa-linkedin"> Linkedin<span className="label">Linkedin</span></a></li>
+										<li><a alt="nothing" href="https://www.linkedin.com/in/ivan-neto/" className="icon brands fa-linkedin"> Linkedin<span className="label">Linkedin</span></a></li>
 									</ul>
 								</header>
 
