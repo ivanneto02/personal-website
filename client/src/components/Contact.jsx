b34814bf6852d08318f3e64f336fcfff0846d352
Ivan Neto <ivan.a.neto@hotmail.com>
2024-08-29 08:46:46 -0700

    Modifying contact info

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -10,7 +10,6 @@ class Contact extends Component {
                 <p>I check my email on a daily basis, almost every hour! Please do not hesistate to contact me if you have any questions or propositions, as I respond very quickly and would love to hear from you!</p>
                 <ul className="contact">
                     <li className="icon solid fa-envelope"><a href="mailto:ivan.a.neto@email.ucr.edu">ivan.neto@email.ucr.edu</a></li>
-                    <li className="icon solid fa-phone">(909) 419-2199</li>
                     <li className="icon solid fa-home"><a href="https://www.google.com/maps/place/Riverside,+CA/">Riverside, California, United States</a></li>
                     <li className="icon brands fa-linkedin"><a href="https://www.linkedin.com/in/ivan-neto/">Linkedin</a><br /></li>
                     <li className="icon fa-file"><a href="https://docs.google.com/document/d/1jAV2I_vfuYwBs0TQp1K_vSsTDDuy1b4_SeKbBFXEaEc/edit?usp=sharing">Resume</a><br /></li>
