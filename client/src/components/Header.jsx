b8e79c058c1a5695de61f6e622c1e5e93c4833c5
ivanneto02 <ivan.a.neto@hotmail.com>
2024-10-27 18:11:48 -0700

    Hotfix build error
    rel="noreferrer" after target="_blank"

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,8 +9,8 @@ class Header extends Component {
         return (
             <header id="header">
                 <ul className="icons">
-                    <li><a href="https://docs.google.com/document/d/1WJInUWfr5vqFRe2XMcU9oozOaHX8jTv-4adBut4mvHQ/edit?usp=sharing" target="_blank" className="icon fa-file"> Resume<span className="label">Resume</span></a></li>
-                    <li><a href="https://www.linkedin.com/in/ivan-neto/" className="icon brands fa-linkedin" target="_blank"> Linkedin<span className="label">Linkedin</span></a></li>
+                    <li><a href="https://docs.google.com/document/d/1WJInUWfr5vqFRe2XMcU9oozOaHX8jTv-4adBut4mvHQ/edit?usp=sharing" target="_blank" rel="noreferrer" className="icon fa-file"> Resume<span className="label">Resume</span></a></li>
+                    <li><a href="https://www.linkedin.com/in/ivan-neto/" className="icon brands fa-linkedin" target="_blank" rel="noreferrer"> Linkedin<span className="label">Linkedin</span></a></li>
                 </ul>
                 <Avatar/>
             </header>
