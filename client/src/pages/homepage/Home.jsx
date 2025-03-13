5db6dcc84405bd19690320a1286e54cc8298de4b
Ivan Neto <ivan.a.neto@hotmail.com>
2025-03-13 13:39:02 -0700

    Refactor for AWS
    - /images/ access now moved to AWS CloudFront url. Useful because now we
      do not need to store our images anywhere special

--- client/src/pages/homepage/Home.jsx
+++ client/src/pages/homepage/Home.jsx
@@ -19,7 +19,7 @@ class Home extends Component {
                     about. */}
                                 <section id="banner">
                                         <div id="banner_image_holder">
-                                                <img id="banner_image" src="/images/pfp.jpg" alt="" />
+                                                <img id="banner_image" src="https://d3otrhxcw2ss5z.cloudfront.net/pfp.jpg" alt="" />
                                                 <header id="banner_image_content">
                                                         <h2>Ivan Neto</h2>
                                                         <p>University of California, Riverside - Alumni</p>
