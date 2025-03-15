4bd87bdd288dabd97fbae7f39f134db52d6320c7
Ivan Neto <ivan.a.neto@hotmail.com>
2025-03-15 04:14:56 -0700

    Modifying the upload and download files
    They will only add/remove files with the same time stamp.

--- client/public/images/sync_upload_script.sh
+++ client/public/images/sync_upload_script.sh
@@ -3,4 +3,4 @@ bucket_name="WebsiteImagesBucket"
 
 echo "Cloning repo/public/images/ into S3 Bucket ${bucket_name}..."
 
-aws s3 sync ./ s3://websiteimagesstack-websiteimagesbucketc3b6307e-ubuqmhoej5as --delete
+aws s3 sync ./ s3://websiteimagesstack-websiteimagesbucketc3b6307e-ubuqmhoej5as --exact-timestamp --delete
