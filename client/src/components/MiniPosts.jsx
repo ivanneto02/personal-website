a6c2d1dcbbfeb621980530e49595efb79684e252
Ivan Neto <ivan.a.neto@hotmail.com>
2025-03-15 04:47:54 -0700

    Changing miniposts
    For now, their height is always 200px, and width is managed
    automatically.
    
    Eventually, I will come up with a more reactive solution

--- client/src/components/MiniPosts.jsx
+++ client/src/components/MiniPosts.jsx
@@ -6,16 +6,16 @@ class MiniPosts extends Component {
                         <section>
                                 <div className="mini-posts">
                                         <article>
-                                                <a href="/" className="image"><img src="/images/img-placeholder.png" alt="" /></a>
-                                                <p>Nothing here yet :)</p>
+                                                <a href="/cs120b" className="image"><img src="https://d3otrhxcw2ss5z.cloudfront.net/videos/cs120b/cs120b_thumbnail.jpg" alt="" /></a>
+                                                <p>CS120 Course Content</p>
                                         </article>
                                         <article>
-                                                <a href="/" className="image"><img src="/images/img-placeholder.png" alt="" /></a>
-                                                <p>Nothing here yet :)</p>
+                                                <a href="/laser-engravings" className="image"><img src="https://d3otrhxcw2ss5z.cloudfront.net/miscellaneous/laser_engraving/machine_view2.jpg" alt="" /></a>
+                                                <p>Laser Engravings</p>
                                         </article>
                                         <article>
-                                                <a href="/" className="image"><img src="/images/img-placeholder.png" alt="" /></a>
-                                                <p>Nothing here yet :)</p>
+                                                <a href="/survival" className="image"><img src="https://d3otrhxcw2ss5z.cloudfront.net/minecraft/villagers/village.png" alt="" /></a>
+                                                <p>Minecraft Survival</p>
                                         </article>
                                 </div>
                         </section>
