b09969c5b21440a3ebcc0749fc94acbf685666b1
ivanneto02 <ivan.a.neto@hotmail.com>
2025-04-01 03:02:50 -0700

    Removed unnecessary menu links
    These are not used yet, best to remove them until they are an actual
    feature!

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -39,25 +39,25 @@ const Menu = () => {
                                         <ul id="submenu">
                                                 <li><Link to="/survival/">Personal Survival</Link></li>
                                                 <li><Link to="/server/">Survival Multiplayer</Link></li>
-                                                <li><Link to="/manacube">Manacube</Link></li>
-                                                <li><Link to="/hypixel">Hypixel</Link></li>
+                                                {/*<li><Link to="/manacube">Manacube</Link></li>*/}
+                                                {/*<li><Link to="/hypixel">Hypixel</Link></li>*/}
                                                 {/* <li><Link to="/modsclients">Mods/Clients</Link></li>
                         <li><Link to="/plugins">Plugins/Servers</Link></li>
                         <li><Link to="/visuals">Visuals</Link></li> */}
                                         </ul>
                                 </li>
-                                <li>
+                                {/*<li>
                                         <span
                                                 className={`opener ${active2 ? "active" : ""}`}
                                                 onClick={() => setActive2(!active2)}
                                         >Projects</span>
                                         <ul id="submenu">
                                                 <li><Link to="/projects">Coming soon</Link></li>
-                                                {/* <li><Link to="/clientstories">Client Stories</Link></li> */}
-                                                {/* <li><Link to="/tipsandtricks">Tips and Tricks</Link></li> */}
-                                                {/* <li><Link to="/monetaryanalysis">Monetary Analysis</Link></li> */}
+                                                <li><Link to="/clientstories">Client Stories</Link></li>
+                                                <li><Link to="/tipsandtricks">Tips and Tricks</Link></li>
+                                                <li><Link to="/monetaryanalysis">Monetary Analysis</Link></li>
                                         </ul>
-                                </li>
+                                </li>*/}
                                 <li>
                                         <span
                                                 className={`opener ${active8 ? "active" : ""}`}
@@ -81,7 +81,7 @@ const Menu = () => {
                                                 <li><Link to="/pets">Pets</Link></li>
                                         </ul>
                                 </li >*/}
-                                <li>
+                                {/*<li>
                                         <span
                                                 className={`opener ${active6 ? "active" : ""}`}
                                                 onClick={() => setActive6(!active6)}
@@ -89,18 +89,18 @@ const Menu = () => {
                                         <ul id="submenu">
                                                 <li><Link to="/research">Coming soon</Link></li>
                                         </ul>
-                                </li>
-                                <li>
+                                </li>*/}
+                                {/*<li>
                                         <span
                                                 className={`opener ${active7 ? "active" : ""}`}
                                                 onClick={() => setActive7(!active7)}
                                         >Reviews</span>
                                         <ul id="submenu">
                                                 <li><Link to="/books">Coming soon</Link></li>
-                                                {/* <li><Link to="/movies">Movies</Link></li> */}
-                                                {/* <li><Link to="/anime">Anime</Link></li> */}
+                                                <li><Link to="/movies">Movies</Link></li>
+                                                <li><Link to="/anime">Anime</Link></li>
                                         </ul>
-                                </li>
+                                </li>*/}
                                 <li>
                                         <span
                                                 className={`opener ${active4 ? "active" : ""}`}
