4a184af96524ba793668126bfcaa8123acab2a7d
Ivan Neto <ivan.a.neto@hotmail.com>
2025-04-13 18:34:26 -0700

    Making title and date cleaner

--- client/src/pages/classes/VideoLoader.jsx
+++ client/src/pages/classes/VideoLoader.jsx
@@ -2,42 +2,44 @@ import { React } from "react";
 import { useState, useEffect } from "react";
 
 function VideoLoader() {
-        const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
-        const load = (response) => {
-                var rows = [];
-                var text = response.split("\n")
-                text.forEach((elem) => {
-                        var items = elem.split(" -=- ");
-                        rows.push({
-                                name: items[0],
-                                id: items[1],
-                                date: items[2],
-                        });
-                });
-                return rows;
-        }
+    const load = (response) => {
+        var rows = [];
+        var text = response.split("\n")
+        text.forEach((elem) => {
+            var items = elem.split(" -=- ");
+            rows.push({
+                name: items[0],
+                id: items[1],
+                date: items[2],
+            });
+        });
+        return rows;
+    }
 
-        useEffect(() => {
-                fetch("/text/cs120b.txt")
-                        .then(response => response.text())
-                        .then(response => response.trim())
-                        .then(response => load(response))
-                        .then(response => setData(response))
-        }, []);
+    useEffect(() => {
+        fetch("/text/cs120b.txt")
+            .then(response => response.text())
+            .then(response => response.trim())
+            .then(response => load(response))
+            .then(response => setData(response))
+    }, []);
 
-        return (
-                <div className="grid">
-                        {data.map((item) => (
-                                <a key={item.id} href={"https://youtu.be/" + item.id} target="_blank" rel="noreferrer" className="griditem emphasis4 card dark">
-                                        <h3>{item.name}</h3>
-                                        <p>Posted: {item.date}</p>
-                                        <img alt={item.name} className="articleImage" src={"http://img.youtube.com/vi/" + item.id + "/0.jpg"}></img>
+    return (
+        <div className="grid">
+            {data.map((item) => (
+                <a key={item.id} href={"https://youtu.be/" + item.id} target="_blank" rel="noreferrer" className="griditem emphasis4 card dark">
+                    <div className="titledate">
+                        <h3>{item.name}</h3>
+                        <p>{item.date}</p>
+                    </div>
+                    <img alt={item.name} className="articleImage" src={"http://img.youtube.com/vi/" + item.id + "/0.jpg"}></img>
 
-                                </a>
-                        ))}
-                </div>
-        );
+                </a>
+            ))}
+        </div>
+    );
 }
 
 export default VideoLoader;
