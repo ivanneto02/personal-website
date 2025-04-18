154e829fbe8a2241db587c52fb312a5cb05038b9
Ivan Neto <ivan.a.neto@hotmail.com>
2025-04-18 00:19:12 -0700

    Adding search bar to mobile sideba

--- client/src/components/SideBarMobile.jsx
+++ client/src/components/SideBarMobile.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 import Menu from "./Menu";
+import { SearchBar } from "@components";
 
 import "@styles/SideBarMobile.css";
 
 const SideBarMobile = () => {
-        return (
-                <div className="sideBarMobile">
-                        <Menu />
-                </div>
-        );
+    return (
+        <div className="sideBarMobile">
+            <SearchBar />
+            <Menu />
+        </div>
+    );
 }
 
 export default SideBarMobile;
